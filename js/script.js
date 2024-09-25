f31de7304674af28bb4fbfe886873e6755f5bd09
Minh Nguyen <nguyenminh.2291@gmail.com>
2024-09-24 20:51:06 -0700

    initial commit

---
+++ js/script.js
@@ -0,0 +1,179 @@
+// Exercise 1
+// let someMonth
+// function theMonth()
+// let currentMonth
+// let summerMonth
+// let myLibraryFunction
+
+// Exercise 2
+// let numLengthStudying = 1
+// let firstName = 'Minh"
+// let blnIsStudent = true
+// let user = null
+
+// Exercise 3
+// let total = 6 + 8
+// let studentFullName = student.firstName + ' ' + student.lastName
+
+// Exercise 4
+// let firstName
+// let strLastName
+// let strAddress
+// let strCity
+// let strState
+// let zipCode
+// let yourAge
+// let referralSource
+// let strMayWeContactYou
+
+// Exercise 5
+// let firstName
+// firstName = 'Minh'
+
+// let strCity = 'San Diego'
+// let strMayWeContactYou = true
+
+// let firstName = 'Minh', strMayWeContactYou = true
+
+// Exercise 6
+// let yearsStudying = 2
+// console.log ('Minh has been studying for ' + yearsStudying + ' years.')
+// Minh has ben studying for 2 years.
+
+// Exercise 7
+// let blnAnswer = true
+// console.log('The answer is: ' + blnAnswer)
+// output:The answer is: true
+
+// let numberValue = 10
+// let blnValue = false
+// console.log(numberValue + blnValue)
+// output: 10
+
+// Exercise 8
+// The string literal is invalid
+// let someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."';
+// console.log(someString);
+
+// Exercise 9
+// let x = null
+// console.log(x)
+// output: null
+
+// let x
+// console.log(x)
+// output: undefined
+
+// Exercise 10
+// String type
+// console.log(typeof 'Hello')
+// Outputs: string
+
+// Number type
+// console.log(typeof 10)
+// Outputs: number
+
+// Boolean type
+// console.log(typeof true)
+// Outputs: boolean 
+
+// Object type
+// console.log(typeof null)
+// Outputs: object
+
+// Undefined type
+// let x
+// console.log(typeof x)
+// Outputs: undefined
+
+// Excercise 11
+// alert('Hello ' + 'Zak Ruvalcaba' + ', welcome to the JavaScript class!')
+
+// alert('Hello ' + 'Minh Nguyen' + ', welcome to the JavaScript class!')
+
+// Excercise 12
+// let scrName = 'Minh Nguyen'
+// alert('Hello ' + scrName + ', welcome to the JavaScript class!')
+
+// Excercise 13
+// let course = 'JavaScript'
+// alert("Welcome to the " + course + " course!")
+
+// Excercise 14
+// let name = 'Zak Ruvalcaba'
+// let course = 'JavaScript'
+// alert('Hello ' + name + '.\nWelcome to the ' + course + ' class!')
+
+// Excercise 15
+// let name = prompt('What is your name?')
+// let course = 'JavaScript'
+// alert('Hello ' + name + '.\nWelcome to the ' + course + ' class!')
+
+// Excercise 16
+// let name = prompt('What is your name?')
+// let course = prompt('What is your class?')
+// alert('Hello ' + name + '.\nWelcome to the ' + course + ' class!')
+
+// Excercise 17
+// let x
+// x = 10
+// let y
+// y = 20
+// console.log(x+y)
+// output: 30
+
+// Exercise 18
+// let x
+// x = 20
+// x += 20
+// console.log(x)
+// output: 40
+
+// Excercise 19
+// let x
+// x = 20
+// x *= 5
+// console.log(x)
+// output: 100
+
+// Exercise 20
+// let x
+// x = 20 % 3
+// x /= 1
+// console.log(x)
+// output: 2
+
+// Exercise 21
+// let x = 6
+// let y = '6'
+// let z = true
+// if(x == y && z === true) {
+//     console.log('They are equal')
+// } 
+// else {
+//     console.log('They are not equal')
+// }
+// output: They are equal
+
+// Excercise 22
+// let x = 6
+// let y = '8'
+// let z = true
+// if(x >= y || z === false) {
+//     console.log('They are correct')
+// } 
+// else {
+//     console.log('They are not correct')
+// } 
+// output:They are not correct
+//
+
+
+
+
+
+
+
+
+
+
